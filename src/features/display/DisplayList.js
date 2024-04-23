05a8967416b3472341ce134e4a027f8983beae87
Thkeya Smith <thkeyasmith@gmail.com>
2024-04-23 15:41:31 -0400

    Restored DisplayList logic

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,23 +1,17 @@
 import { useSelector } from "react-redux";
-import { createSelector } from "reselect"; // Import createSelector
 import { Col, Row } from "reactstrap";
 import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
 
-// Memoize selectors using createSelector
-const selectItems = createSelector(
-  selectFeaturedCampsite,
-  selectFeaturedPromotion,
-  selectFeaturedPartner,
-  (campsite, promotion, partner) => [campsite, promotion, partner]
-);
-
 const DisplayList = () => {
-  const items = useSelector((state) => selectItems(state)); // Use memoized selector
-  
-  console.log('display items:', items);
+  const items = useSelector((state) => [
+    selectFeaturedCampsite(state),
+    selectFeaturedPromotion(state),
+    selectFeaturedPartner(state),
+  ]);
+  console.log("display items:", items);
 
   return (
     <Row>
