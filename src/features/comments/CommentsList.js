b435936d8c7e078b6e75c7f3041014d6ba4b808b
Thkeya Smith <thkeyasmith@gmail.com>
2024-05-04 12:32:02 -0400

    refactored commentslist code

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -13,31 +13,26 @@ const CommentsList = ({ campsiteId }) => {
     const isLoading = useSelector((state) => state.comments.isLoading);
     const errMsg = useSelector((state) => state.comments.errMsg);
 
-    if (comments && comments.length > 0) {
-        return isLoading ? (
-            <Loading />
-        ) : errMsg ? (
-            <Error errMsg={errMsg} />
-        ) : (
-            <Col md='5' className='m-1'>
-                <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />;
-                })}
-                <CommentForm campsiteId={campsiteId} />
-            </Col>
-        );
-    }
     return isLoading ? (
         <Loading />
     ) : errMsg ? (
         <Error errMsg={errMsg} />
     ) : (
         <Col md='5' className='m-1'>
-            There are no comments for this campsite yet.
+            {comments && comments.length > 0 ? (
+                <>
+                    <h4>Comments</h4>
+                    {comments.map((comment) => (
+                        <Comment key={comment.id} comment={comment} />
+                    ))}
+                </>
+            ) : (
+                <div>There are no comments for this campsite yet.</div>
+            )}
             <CommentForm campsiteId={campsiteId} />
         </Col>
     );
-};
+    
+}
 
 export default CommentsList;
